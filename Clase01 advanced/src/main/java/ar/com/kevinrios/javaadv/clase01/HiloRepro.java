0210d7fecde7016df232290f821b17e58758f05a
KevinRios <KevinJIRios@gmail.com>
2018-07-11 10:55:53 -0300

    Add files via upload

--- Clase01 advanced/src/main/java/ar/com/kevinrios/javaadv/clase01/HiloRepro.java
+++ Clase01 advanced/src/main/java/ar/com/kevinrios/javaadv/clase01/HiloRepro.java
@@ -4,9 +4,18 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import javazoom.jl.player.Player;
 
-public class HiloRepro {
+public class HiloRepro implements Runnable{
     
     public static void main(String[] args)throws Exception{
-        new Player(new BufferedInputStream(new FileInputStream("c:condor.mp3"))).play();
+        new Player(new BufferedInputStream(new FileInputStream("resources/musica.mp3"))).play();
+    }
+
+    @Override
+    public void run() {
+        try {
+            new Player(new BufferedInputStream(new FileInputStream("resources/musica.mp3"))).play();    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
